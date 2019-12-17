689f16f27404877aadc59757cc630c59b83eba14
Aptineeva <58913450+Aptineeva@users.noreply.github.com>
2019-12-17 19:02:20 +0300

    Add files via upload

---
+++ secondTry.py
@@ -0,0 +1,30 @@
+import telebot
+import os
+import time
+
+bot = telebot.TeleBot('947227617:AAEBlhomRlRuqeTymeM-zVo9Pn6JmxwgVf4')
+keyboard = telebot.types.ReplyKeyboardMarkup()
+keyboard.row('Привет', 'Пока', 'ты лох?')
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, хочешь поговорить?', reply_markup=keyboard)
+
+
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, 'Приветос')
+    elif message.text.lower() == 'пока':
+        bot.send_message(message.chat.id, 'Прощай......я буду скучать')
+    elif message.text.lower() == 'ты лох?':
+        bot.send_message(message.chat.id, 'сам ты лох, дебил')
+    else:
+        bot.send_sticker(message.chat.id, 'CAADAgADNAADkp8eEfIdTHZlMTXQFgQ')
+
+
+
+bot.polling(none_stop = True, interval = 0)
+
+
