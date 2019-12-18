8a092d52756c58195125a6996859e3426d15cb07
Aptineeva <58913450+Aptineeva@users.noreply.github.com>
2019-12-18 22:54:29 +0300

    Update secondTry.py

--- secondTry.py
+++ secondTry.py
@@ -10,7 +10,7 @@ bot = telebot.TeleBot('947227617:AAEBlhomRlRuqeTymeM-zVo9Pn6JmxwgVf4')
 
 keyboard_mood = telebot.types.ReplyKeyboardMarkup()
 keyboard_mood.row('New Year', 'In love', 'Gym', 'Sad')
-keyboard_mood.row('Party', 'Childhood', 'Studying')
+keyboard_mood.row('Party', 'Nostalgia', 'Studying')
 keyboard_mood.row('Poetry', 'Melancholy', 'Назад')
 
 keyboard_menu = telebot.types.ReplyKeyboardMarkup()
@@ -25,7 +25,8 @@ keyboard_genre.row('Инди', 'Блюз-рок', 'Назад')
 
 NewYear_id = ['CQADAgADfgUAAkn8yEuQTb7jsefMwRYE', 'CQADAgADuQMAAlB-0Esk5DfJL3PL3xYE', 'CQADAgADugMAAlB-0EsKKBL745odthYE',
              'CQADAgADiAUAAlHpyEtHo19BOckZBhYE', 'CQADAgADigUAAlHpyEsncQwqd_wN-hYE', 'CQADAgADEgUAAuKn0Evq-j1RUa4aERYE',
-             'CQADAgADDgYAAkn80EtWBclWfG6KhhYE', 'CQADAgADDwYAAkn80Evvwca7YbFaoBYE', 'CQADAgADEAYAAkn80Euh1lFcu-MJNBYE']
+             'CQADAgADDgYAAkn80EtWBclWfG6KhhYE', 'CQADAgADDwYAAkn80Evvwca7YbFaoBYE', 'CQADAgADEAYAAkn80Euh1lFcu-MJNBYE',
+             'CQADAgADzwYAAkn80Etwrhhg48UHVhYE', 'CQADAgAD0AYAAkn80EsKVrpSlJGvPhYE', 'CQADAgADWwQAAuKn2Evvw1mMqBEBZBYE']
 
 InLove_id = ['CQADAgADJgQAAlB-0Es96dEgDKGQghYE', 'CQADAgADGQYAAkn80EvnNZ9FNewtwxYE', 'CQADAgADGwYAAkn80EuN3DNk4QMXhxYE',
           'CQADAgADFgUAAuKn0Etzpel0EkiqURYE', 'CQADAgADGgUAAuKn0EsBD2q95fkLaRYE', 'CQADAgADIwYAAkn80Ev1DMxu78JQXhYE',
@@ -34,6 +35,27 @@ InLove_id = ['CQADAgADJgQAAlB-0Es96dEgDKGQghYE', 'CQADAgADGQYAAkn80EvnNZ9FNewtwx
           'CQADAgADKgYAAkn80EuSp890BjtcnhYE', 'CQADAgADKwYAAkn80Ev_cdb_6Ylm4RYE', 'CQADAgADLAYAAkn80EtAhTYEUxHk0xYE',
           'CQADAgADJAUAAuKn0EsOjFP7zHSMGxYE', 'CQADAgADJQUAAuKn0EvNjca2H-iMVxYE', 'CQADAgADLQYAAkn80EsmWwg4rNxR8xYE',
           'CQADAgADJwUAAuKn0EuH_xVjSLMGERYE', 'CQADAgADLwYAAkn80EtQPbwwElckAAEWBA', 'CQADAgADKQQAAlB-0Etro6O-CxIXqhYE']
+
+Gym_id = ['CQADAgADaAQAAuKn2Ev6bqsxt0Z9LhYE', 'CQADAgADaQQAAuKn2EvKeDre7P00wRYE', 'CQADAgADagQAAuKn2EsNZKbj2w55FxYE',
+          'CQADAgAD2QYAAkn80EvOWq-vhPwrnRYE', 'CQADAgADbgQAAuKn2EspMN9EhoWJUxYE', 'CQADAgAD2wYAAkn80Et8mVrPnEoyyBYE',
+          'CQADAgADcQQAAuKn2EvxxDRi1xLq-xYE', 'CQADAgADhAQAAl_02UsxEX7mDgRL5RYE', 'CQADAgAD3AYAAkn80EvGTNAY4KErPBYE',
+          'CQADAgAD3QYAAkn80EvQ13Zk_o8HaRYE', 'CQADAgADdQQAAuKn2EtDf6wHikeXvxYE', 'CQADAgADdgQAAuKn2EslNtgy_6fp0RYE',
+          'CQADAgADeAQAAuKn2Etqv6ihz5LMXRYE', 'CQADAgADegQAAuKn2EtRt-NRDpb5XxYE', 'CQADAgADhgQAAl_02Ut1DFAy6WP96RYE',
+          'CQADAgAD4AYAAkn80EusWEgzGgVX2xYE']
+
+Sad_id =[]
+
+Party_id = []
+
+Nostalgia_id = []
+
+Studying_id =[]
+
+Poetry_id = []
+
+Melancholy_id = []
+
+
 
 # Сделать листы для остальных песен
 
@@ -51,15 +73,15 @@ def start_message(message):
 
 @bot.message_handler(commands=['song'])
 def find_file_ids(message):
-    for file in os.listdir('/Users/elinaaptineeva/Desktop/music_bot/in.love/'):
+    for file in os.listdir('/Users/elinaaptineeva/Desktop/gym/'):
         if file.split('.')[-1] == 'm4a' or 'mp3':
-            songg = open('/Users/elinaaptineeva/Desktop/music_bot/in.love/'+file, 'rb')
+            songg = open('/Users/elinaaptineeva/Desktop/gym/'+file, 'rb')
             msg = bot.send_audio(message.chat.id, songg, None)
             bot.send_message(message.chat.id, msg.audio.file_id, reply_to_message_id=msg.message_id)
-        time.sleep(20)
+        time.sleep(30)
 
 
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(func=lambda message: True, content_types=['text']) #Все в одной функции, так как все сообщения имеют один тип text
 def send_text(message):
     if message.text.lower() == 'привет':
         bot.send_message(message.chat.id, 'Приветос')
@@ -67,6 +89,8 @@ def send_text(message):
         bot.send_message(message.chat.id, 'Прощай......я буду скучать')
     elif message.text.lower() == 'назад':
         bot.send_message(message.chat.id, 'Как ты хочешь осуществить выбор музыки?', reply_markup=keyboard_menu)
+
+        #Выбор для настроения
     elif message.text.lower() == 'по настроению':
         bot.send_message(message.chat.id, 'Какое у тебя настроение?', reply_markup=keyboard_mood)
     elif message.text.lower() == 'new year':
@@ -75,18 +99,75 @@ def send_text(message):
         bot.send_audio(message.chat.id, random.choice(NewYear_id), disable_notification=1)
         bot.send_audio(message.chat.id, random.choice(NewYear_id), disable_notification=1)
         bot.send_audio(message.chat.id, random.choice(NewYear_id), disable_notification=1)
+        bot.send_sticker(message.chat.id, 'CAADAgADEAADkp8eEQXW_GnuhWb_FgQ')
+
     elif message.text.lower() == 'in love':
         bot.send_audio(message.chat.id, random.choice(InLove_id), disable_notification=1)
         bot.send_audio(message.chat.id, random.choice(InLove_id), disable_notification=1)
         bot.send_audio(message.chat.id, random.choice(InLove_id), disable_notification=1)
         bot.send_audio(message.chat.id, random.choice(InLove_id), disable_notification=1)
         bot.send_audio(message.chat.id, random.choice(InLove_id), disable_notification=1)
+
+    elif message.text.lower() == 'gym':
+        bot.send_audio(message.chat.id, random.choice(Gym_id), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(Gym_id), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(Gym_id), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(Gym_id), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(Gym_id), disable_notification=1)
+        bot.send_sticker(message.chat.id, 'CAADAgADXgADaUCAC_ik5ou43wUkFgQ')
+
+    elif message.text.lower() == 'sad':
+        bot.send_audio(message.chat.id, random.choice(), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(), disable_notification=1)
+        bot.send_sticker(message.chat.id, '')
+
+    elif message.text.lower() == 'party':
+        bot.send_audio(message.chat.id, random.choice(), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(), disable_notification=1)
+        bot.send_sticker(message.chat.id, '')
+
+    elif message.text.lower() == 'nostalgia':
+        bot.send_audio(message.chat.id, random.choice(), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(), disable_notification=1)
+        bot.send_sticker(message.chat.id, '')
+
+    elif message.text.lower() == 'studying':
+        bot.send_audio(message.chat.id, random.choice(), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(), disable_notification=1)
+        bot.send_sticker(message.chat.id, '')
+
+    elif message.text.lower() == 'poetry':
+        bot.send_audio(message.chat.id, random.choice(), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(), disable_notification=1)
+        bot.send_sticker(message.chat.id, '')
+
+    elif message.text.lower() == 'melancholy':
+        bot.send_audio(message.chat.id, random.choice(), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(), disable_notification=1)
+        bot.send_sticker(message.chat.id, '')
+        
+        #Выбор для жанра
+        
     else:
         bot.send_sticker(message.chat.id, 'CAADAgADNAADkp8eEfIdTHZlMTXQFgQ')
-
-
-
-
 
 
 bot.polling(none_stop = True, interval = 0)
