af67acc0c1f84822b53f2b80d46c3400aa4b97a0
Aptineeva <58913450+Aptineeva@users.noreply.github.com>
2019-12-19 15:08:19 +0300

    Update secondTry.py

--- secondTry.py
+++ secondTry.py
@@ -4,7 +4,6 @@ import time
 import random
 import openpyxl
 
-
 bot = telebot.TeleBot('947227617:AAEBlhomRlRuqeTymeM-zVo9Pn6JmxwgVf4')
 
 workbook = openpyxl.load_workbook(filename="/Users/elinaaptineeva/Desktop/Music.xlsx")
@@ -25,22 +24,20 @@ acoustics_sheet = workbook["Acoustics_id"]
 
 ny_list = [ny_sheet.cell(row=i,column=1).value for i in range(1,13)]
 love_list = [love_sheet.cell(row=i,column=1).value for i in range(1,21)]
-gym_list = [gym_sheet.cell(row=i,column=1).value for i in range(1,21)]
+gym_list = [gym_sheet.cell(row=i,column=1).value for i in range(1,16)]
 sad_list = [sad_sheet.cell(row=i,column=1).value for i in range(1,21)]
-party_list = [party_sheet.cell(row=i,column=1).value for i in range(1,21)]
-nostalgia_list = [nostalgia_sheet.cell(row=i,column=1).value for i in range(1,21)]
-studying_list = [studying_sheet.cell(row=i,column=1).value for i in range(1,21)]
-pop_list = [pop_sheet.cell(row=i,column=1).value for i in range(1,21)]
-alternative_list = [alternative_sheet.cell(row=i,column=1).value for i in range(1,21)]
-rock_list = [rock_sheet.cell(row=i,column=1).value for i in range(1,21)]
+party_list = [party_sheet.cell(row=i,column=1).value for i in range(1,19)]
+nostalgia_list = [nostalgia_sheet.cell(row=i,column=1).value for i in range(1,12)]
+studying_list = [studying_sheet.cell(row=i,column=1).value for i in range(1,10)]
+pop_list = [pop_sheet.cell(row=i,column=1).value for i in range(1,16)]
+alternative_list = [alternative_sheet.cell(row=i,column=1).value for i in range(1,13)]
+rock_list = [rock_sheet.cell(row=i,column=1).value for i in range(1,18)]
 randb_list = [randb_sheet.cell(row=i,column=1).value for i in range(1,21)]
 electro_list = [electro_sheet.cell(row=i,column=1).value for i in range(1,21)]
 sleep_list = [sleep_sheet.cell(row=i,column=1).value for i in range(1,21)]
 acoustics_list = [acoustics_sheet.cell(row=i,column=1).value for i in range(1,21)]
 #birthday_list = рандом из всех листов
 #top просто 5 наших песен
-
-
 
 
 #keyboard = telebot.types.ReplyKeyboardMarkup()
@@ -61,87 +58,6 @@ keyboard_genre.row('Pop', 'Альтернатива')
 keyboard_genre.row('Рок', 'R&B', 'Электроника')
 keyboard_genre.row('Назад')
 
-#Списки file_id аудио для настроения
-
-NewYear_id = ['CQADAgADfgUAAkn8yEuQTb7jsefMwRYE', 'CQADAgADuQMAAlB-0Esk5DfJL3PL3xYE', 'CQADAgADugMAAlB-0EsKKBL745odthYE',
-             'CQADAgADiAUAAlHpyEtHo19BOckZBhYE', 'CQADAgADigUAAlHpyEsncQwqd_wN-hYE', 'CQADAgADEgUAAuKn0Evq-j1RUa4aERYE',
-             'CQADAgADDgYAAkn80EtWBclWfG6KhhYE', 'CQADAgADDwYAAkn80Evvwca7YbFaoBYE', 'CQADAgADEAYAAkn80Euh1lFcu-MJNBYE',
-             'CQADAgADzwYAAkn80Etwrhhg48UHVhYE', 'CQADAgAD0AYAAkn80EsKVrpSlJGvPhYE', 'CQADAgADWwQAAuKn2Evvw1mMqBEBZBYE']
-
-InLove_id = ['CQADAgADJgQAAlB-0Es96dEgDKGQghYE', 'CQADAgADGQYAAkn80EvnNZ9FNewtwxYE', 'CQADAgADGwYAAkn80EuN3DNk4QMXhxYE',
-          'CQADAgADFgUAAuKn0Etzpel0EkiqURYE', 'CQADAgADGgUAAuKn0EsBD2q95fkLaRYE', 'CQADAgADIwYAAkn80Ev1DMxu78JQXhYE',
-          'CQADAgADJAYAAkn80EsnID9m4-LQDRYE', 'CQADAgADJgYAAkn80EuDlBByM1LzphYE', 'CQADAgADJwYAAkn80Et4CCNhik8t4RYE',
-          'CQADAgADKAYAAkn80Eshm3ICw0V4kxYE', 'CQADAgADHwUAAuKn0EvxUqwOz4SkVRYE', 'CQADAgADKQYAAkn80EtATnQVRpPWhxYE',
-          'CQADAgADKgYAAkn80EuSp890BjtcnhYE', 'CQADAgADKwYAAkn80Ev_cdb_6Ylm4RYE', 'CQADAgADLAYAAkn80EtAhTYEUxHk0xYE',
-          'CQADAgADJAUAAuKn0EsOjFP7zHSMGxYE', 'CQADAgADJQUAAuKn0EvNjca2H-iMVxYE', 'CQADAgADLQYAAkn80EsmWwg4rNxR8xYE',
-          'CQADAgADJwUAAuKn0EuH_xVjSLMGERYE', 'CQADAgADLwYAAkn80EtQPbwwElckAAEWBA', 'CQADAgADKQQAAlB-0Etro6O-CxIXqhYE']
-
-Gym_id = ['CQADAgADaAQAAuKn2Ev6bqsxt0Z9LhYE', 'CQADAgADaQQAAuKn2EvKeDre7P00wRYE', 'CQADAgADagQAAuKn2EsNZKbj2w55FxYE',
-          'CQADAgAD2QYAAkn80EvOWq-vhPwrnRYE', 'CQADAgADbgQAAuKn2EspMN9EhoWJUxYE', 'CQADAgAD2wYAAkn80Et8mVrPnEoyyBYE',
-          'CQADAgADcQQAAuKn2EvxxDRi1xLq-xYE', 'CQADAgADhAQAAl_02UsxEX7mDgRL5RYE', 'CQADAgAD3AYAAkn80EvGTNAY4KErPBYE',
-          'CQADAgAD3QYAAkn80EvQ13Zk_o8HaRYE', 'CQADAgADdQQAAuKn2EtDf6wHikeXvxYE', 'CQADAgADdgQAAuKn2EslNtgy_6fp0RYE',
-          'CQADAgADeAQAAuKn2Etqv6ihz5LMXRYE', 'CQADAgADegQAAuKn2EtRt-NRDpb5XxYE', 'CQADAgADhgQAAl_02Ut1DFAy6WP96RYE',
-          'CQADAgAD4AYAAkn80EusWEgzGgVX2xYE']
-
-Sad_id =['CQADAgADrAQAAuKn2EvTYXbGCJhA_xYE', 'CQADAgADBAcAAkn80Eu8SpLIeqpjVRYE', 'CQADAgADsQQAAuKn2EtPdwj4_rVMVRYE',
-         'CQADAgADBQcAAkn80Eu-KlWLkYBKQxYE', 'CQADAgADBwcAAkn80EvUDno6_qyRAxYE', 'CQADAgADtgQAAuKn2EtKyZd94LoUNBYE',
-         'CQADAgADCQcAAkn80Eu3xJ4naMt1KxYE', 'CQADAgADuQQAAuKn2EseihnnQzqJlhYE', 'CQADAgADDAcAAkn80EvZBEsNzI-E2BYE',
-         'CQADAgADvQQAAuKn2Esn6g1_-AzhpBYE', 'CQADAgADEAcAAkn80EvpdV9TvYllJhYE', 'CQADAgADEgcAAkn80EtjuwxR1GsjYBYE',
-         'CQADAgADEwcAAkn80EtsP4Xs3euWYBYE', 'CQADAgADvwQAAuKn2Evre264AUZrVBYE', 'CQADAgADFQcAAkn80EtLifRoZpPlqBYE',
-         'CQADAgADFgcAAkn80Es8hE7yupJumhYE', 'CQADAgADxAQAAuKn2EusuYR858FlOhYE', 'CQADAgADxQQAAuKn2Es9yEUAAbsZd3wWBA',
-         'CQADAgADxgQAAuKn2EtgsAY-Qk2NTBYE', 'CQADAgADkwQAAl_02UtMpXeZxbwPJhYE', 'CQADAgADFwcAAkn80EsnFRJnwFEHIBYE']
-
-Party_id = ['CQADAgADGgcAAkn80EunO4qC21oXhBYE', 'CQADAgADGwcAAkn80EtJ0tQioTcD1hYE', 'CQADAgADlQQAAl_02UtuDBWmwRopUxYE',
-            'CQADAgADHAcAAkn80Eu9bwJVR4ONqhYE', 'CQADAgADHQcAAkn80EuAuKC_-C8PXBYE', 'CQADAgADHgcAAkn80EsjZJRFC9YiHhYE',
-            'CQADAgADlgQAAl_02UtiTCZRYMOowBYE', 'CQADAgADHwcAAkn80Eu_dA0jrXvWyBYE', 'CQADAgADzQQAAuKn2EvGkvLqAvIzqhYE',
-            'CQADAgADIAcAAkn80Es5oygtutY19hYE', 'CQADAgADIQcAAkn80Et6fzZARseZRBYE', 'CQADAgADIgcAAkn80EuaqluAR101-xYE',
-            'CQADAgADzwQAAuKn2EtojAqCHL7HKRYE', 'CQADAgADJAcAAkn80EsYPQG2EJqNuRYE', 'CQADAgADJQcAAkn80EuttYM5UseC6BYE',
-            'CQADAgAD0QQAAuKn2EuVThPRTk1sHBYE', 'CQADAgADKAcAAkn80EvZhTykoLc6KBYE', 'CQADAgADKgcAAkn80EuwpoemfskDhBYE',
-            'CQADAgADLAcAAkn80EuEUDMIKx8qrRYE']
-
-Nostalgia_id = ['CQADAgADNgcAAkn80EsMpozYaTayqxYE', 'CQADAgAD3AQAAuKn2Es9-Rq8QhvTwBYE', 'CQADAgAD3QQAAuKn2EtmjelNeBXGlxYE',
-                'CQADAgAD3gQAAuKn2EszL3SVFwYDgxYE', 'CQADAgAD3wQAAuKn2EsUEep5yAFl6BYE', 'CQADAgADQAcAAkn80EvGkSNLFzkPlxYE',
-                'CQADAgADQwcAAkn80Evh_Zac0CuMshYE', 'CQADAgAD4QQAAuKn2EujCjoU3xhyIxYE', 'CQADAgADRQcAAkn80Etl5AsSgTmwuhYE',
-                'CQADAgADsAQAAl_02Usf3V1DPv82ExYE', 'CQADAgAD4gQAAuKn2EtsL8Y_WWDedxYE', 'CQADAgADRgcAAkn80Evd-ayXhFJyuhYE']
-
-Studying_id =['CQADAgADSAcAAkn80Evzdad4kE0p0hYE', 'CQADAgADSQcAAkn80Eu7RP4eGL6DXxYE', 'CQADAgADtgQAAl_02Utkt0Mjl5QadxYE',
-              'CQADAgADuQQAAl_02Uv78DwkidCO8RYE', 'CQADAgADSwcAAkn80EsZqsqFXp78VhYE', 'CQADAgADUAcAAkn80EtbweZDkY1wuxYE',
-              'CQADAgADUgcAAkn80EuP7Yt9EriJQhYE', 'CQADAgADUwcAAkn80EsvkikOY98L7BYE', 'CQADAgADxAQAAl_02Uu34zFiPidIihYE',
-              'CQADAgADVAcAAkn80Evb-JkvxNRGAxYE']
-
-#Списки file_id аудио для жанров
-
-Pop_id = ['CQADAgADVwcAAkn80EvnSJwmIolkghYE', 'CQADAgADWAcAAkn80EttsuWE73G6NxYE', 'CQADAgADWQcAAkn80Esaw8VDCNgQuBYE',
-          'CQADAgAD5gQAAuKn2Es0_syZYRE_nRYE', 'CQADAgADWwcAAkn80EsEDFAbSm432xYE', 'CQADAgAD5wQAAuKn2EsCiw3vA-br0RYE',
-          'CQADAgAD6AQAAuKn2Et3aVL--o7eqRYE', 'CQADAgADXQcAAkn80EtrdD_P_X274BYE', 'CQADAgADXgcAAkn80EvVM-VTHwgvQRYE',
-          'CQADAgADXwcAAkn80Et2aXZMBintnBYE', 'CQADAgAD-gQAArqK2EsNeOWky-VmtxYE', 'CQADAgADYAcAAkn80EvpWKwyAgmOmRYE',
-          'CQADAgADGAQAAuGH2EsJ9eW7Crr_UxYE', 'CQADAgADGQQAAuGH2EuRvX4Q7rec5BYE', 'CQADAgAD2AQAAl_02UuGfuUoqvNvtRYE',
-          'CQADAgADGgQAAuGH2EtX58BlyBudpBYE']
-
-Alternative_id = ['CQADAgADYwUAAuKn2Es-MiWHIwqmoBYE', 'CQADAgADGwUAAl_02Uth9nw0AW9qRxYE', 'CQADAgADRAQAAuGH2EuTp8kCersoqhYE',
-                  'CQADAgADHgUAAl_02Uvuhw2XbVCuTBYE', 'CQADAgADRQQAAuGH2Eu4kys_Qe-vCBYE', 'CQADAgADRwQAAuGH2Et7FZgboBB5thYE',
-                  'CQADAgADJQUAAl_02UvS-KPf-Knk1RYE', 'CQADAgADbgUAAuKn2EtEfsYkEOI2ThYE', 'CQADAgADTAQAAuGH2EuZTcx_EWMmFxYE',
-                  'CQADAgADJwUAAl_02Ut9RxprO7VnaBYE', 'CQADAgADcAUAAuKn2Et1P683h7JkMRYE', 'CQADAgADTQQAAuGH2EtK09FwHW6I_hYE',
-                  'CQADAgADTgQAAuGH2Eslw0CAW_QtsRYE']
-
-Rock_id = ['CQADAgADUAQAAuGH2EvCd-q2s53z0xYE', 'CQADAgADcQUAAuKn2EuDmsUGmwNWORYE', 'CQADAgADKQUAAl_02UvI8AzTtv77wxYE',
-           'CQADAgADdAUAAuKn2EvYjMpLAAGOW0oWBA', 'CQADAgADdQUAAuKn2EtQmLMnJvxp1hYE', 'CQADAgADUgQAAuGH2Eu1L6wm_pIrxRYE',
-           'CQADAgADdwUAAuKn2EvY4IKsT0miKhYE', 'CQADAgADUwQAAuGH2EvOPGQ1cZODsBYE', 'CQADAgADeAUAAuKn2Et9FDj56wKmgBYE',
-           'CQADAgADeQUAAuKn2EtPZDLO5syW7RYE', 'CQADAgADLwUAAl_02UsoJAkbMq-sqxYE', 'CQADAgADVQQAAuGH2Esd9sl00tq2vRYE',
-           'CQADAgADfAUAAuKn2Eu3ff_BTfirRBYE', 'CQADAgADVgQAAuGH2Evr6_fyJ7mwHBYE', 'CQADAgADVwQAAuGH2EvfOYyySbbC7hYE',
-           'CQADAgADfgUAAuKn2EuypwpO5ZGzxxYE', 'CQADAgADgAUAAuKn2Es-_Mh10DmlIRYE','CQADAgADWAQAAuGH2EvU4l208Pv_LBYE']
-
-RandB_id = []
-
-Electro_id = []
-
-#Списки file_id аудио для сна
-
-ForSleep_id = []
-
-#Списки file_id для акустики
-
-Acoustics_id = []
 
 #new_list = random.sample(Christmas_id, k=5)
 #def random_songs():
@@ -178,57 +94,55 @@ def send_text(message):
     elif message.text.lower() == 'по настроению':
         bot.send_message(message.chat.id, 'Какое у тебя настроение?', reply_markup=keyboard_mood)
     elif message.text.lower() == 'new year':
-        bot.send_audio(message.chat.id, random.choice(NewYear_id), disable_notification=1) #не нашли функцию, позволяющюю отправлять несколько аудио в одном сообщении,
-        bot.send_audio(message.chat.id, random.choice(NewYear_id), disable_notification=1) #поэтому приходится пять раз отправлять разные сообщения
-        bot.send_audio(message.chat.id, random.choice(NewYear_id), disable_notification=1)
-        bot.send_audio(message.chat.id, random.choice(NewYear_id), disable_notification=1)
-        bot.send_audio(message.chat.id, random.choice(NewYear_id), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(ny_list), disable_notification=1) #не нашли функцию, позволяющюю отправлять несколько аудио в одном сообщении,
+        bot.send_audio(message.chat.id, random.choice(ny_list), disable_notification=1) #поэтому приходится пять раз отправлять разные сообщения
+        bot.send_audio(message.chat.id, random.choice(ny_list), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(ny_list), disable_notification=1)
         bot.send_sticker(message.chat.id, 'CAADAgADEAADkp8eEQXW_GnuhWb_FgQ')
 
     elif message.text.lower() == 'in love':
-        bot.send_audio(message.chat.id, random.choice(InLove_id), disable_notification=1)
-        bot.send_audio(message.chat.id, random.choice(InLove_id), disable_notification=1)
-        bot.send_audio(message.chat.id, random.choice(InLove_id), disable_notification=1)
-        bot.send_audio(message.chat.id, random.choice(InLove_id), disable_notification=1)
-        bot.send_audio(message.chat.id, random.choice(InLove_id), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(love_list), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(love_list), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(love_list), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(love_list), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(love_list), disable_notification=1)
         bot.send_sticker(message.chat.id, 'CAADAgADBAADkp8eEavc7j2rScUkFgQ')
 
     elif message.text.lower() == 'gym':
-        bot.send_audio(message.chat.id, random.choice(Gym_id), disable_notification=1)
-        bot.send_audio(message.chat.id, random.choice(Gym_id), disable_notification=1)
-        bot.send_audio(message.chat.id, random.choice(Gym_id), disable_notification=1)
-        bot.send_audio(message.chat.id, random.choice(Gym_id), disable_notification=1)
-        bot.send_audio(message.chat.id, random.choice(Gym_id), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(gym_list), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(gym_list), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(gym_list), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(gym_list), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(gym_list), disable_notification=1)
         bot.send_sticker(message.chat.id, 'CAADAgADXgADaUCAC_ik5ou43wUkFgQ')
 
     elif message.text.lower() == 'sad':
-        bot.send_audio(message.chat.id, random.choice(Sad_id), disable_notification=1)
-        bot.send_audio(message.chat.id, random.choice(Sad_id), disable_notification=1)
-        bot.send_audio(message.chat.id, random.choice(Sad_id), disable_notification=1)
-        bot.send_audio(message.chat.id, random.choice(Sad_id), disable_notification=1)
-        bot.send_audio(message.chat.id, random.choice(Sad_id), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(sad_list), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(sad_list), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(sad_list), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(sad_list), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(sad_list), disable_notification=1)
         bot.send_sticker(message.chat.id, 'CAADAQADoBIAApl_iALzktRqOg9uhhYE')
 
     elif message.text.lower() == 'party':
-        bot.send_audio(message.chat.id, random.choice(Party_id), disable_notification=1)
-        bot.send_audio(message.chat.id, random.choice(Party_id), disable_notification=1)
-        bot.send_audio(message.chat.id, random.choice(Party_id), disable_notification=1)
-        bot.send_audio(message.chat.id, random.choice(Party_id), disable_notification=1)
-        bot.send_audio(message.chat.id, random.choice(Party_id), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(party_list), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(party_list), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(party_list), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(party_list), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(party_list), disable_notification=1)
         bot.send_sticker(message.chat.id, 'CAADAgADowADtvArFPC6O5csMnkcFgQ')
 
     elif message.text.lower() == 'nostalgia':
-        bot.send_audio(message.chat.id, random.choice(Nostalgia_id), disable_notification=1)
-        bot.send_audio(message.chat.id, random.choice(Nostalgia_id), disable_notification=1)
-        bot.send_audio(message.chat.id, random.choice(Nostalgia_id), disable_notification=1)
-        bot.send_audio(message.chat.id, random.choice(Nostalgia_id), disable_notification=1)
-        bot.send_audio(message.chat.id, random.choice(Nostalgia_id), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(nostalgia_list), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(nostalgia_list), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(nostalgia_list), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(nostalgia_list), disable_notification=1)
         #bot.send_sticker(message.chat.id, '')
 
     elif message.text.lower() == 'studying':
-        bot.send_audio(message.chat.id, random.choice(Studying_id), disable_notification=1)
-        bot.send_audio(message.chat.id, random.choice(Studying_id), disable_notification=1)
-        bot.send_audio(message.chat.id, random.choice(Studying_id), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(studying_list), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(studying_list), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(studying_list), disable_notification=1)
         bot.send_sticker(message.chat.id, 'CAADAgADFAkAAlwCZQOykwORp3AchRYE')
 
     elif message.text.lower() == 'poetry':
@@ -243,27 +157,26 @@ def send_text(message):
     elif message.text.lower() == 'по жанрам':
         bot.send_message(message.chat.id, 'Какой ты хочешь жанр?', reply_markup=keyboard_genre)
     elif message.text.lower() == 'pop':
-        bot.send_audio(message.chat.id, random.choice(Pop_id), disable_notification=1)
-        bot.send_audio(message.chat.id, random.choice(Pop_id), disable_notification=1)
-        bot.send_audio(message.chat.id, random.choice(Pop_id), disable_notification=1)
-        bot.send_audio(message.chat.id, random.choice(Pop_id), disable_notification=1)
-        bot.send_audio(message.chat.id, random.choice(Pop_id), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(pop_list), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(pop_list), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(pop_list), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(pop_list), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(pop_list), disable_notification=1)
         #bot.send_sticker(message.chat.id, '')
 
     elif message.text.lower() == 'альтернатива':
-        bot.send_audio(message.chat.id, random.choice(Alternative_id), disable_notification=1)
-        bot.send_audio(message.chat.id, random.choice(Alternative_id), disable_notification=1)
-        bot.send_audio(message.chat.id, random.choice(Alternative_id), disable_notification=1)
-        bot.send_audio(message.chat.id, random.choice(Alternative_id), disable_notification=1)
-        bot.send_audio(message.chat.id, random.choice(Alternative_id), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(alternative_list), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(alternative_list), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(alternative_list), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(alternative_list), disable_notification=1)
         #bot.send_sticker(message.chat.id, '')
 
     elif message.text.lower() == 'рок':
-        bot.send_audio(message.chat.id, random.choice(Rock_id), disable_notification=1)
-        bot.send_audio(message.chat.id, random.choice(Rock_id), disable_notification=1)
-        bot.send_audio(message.chat.id, random.choice(Rock_id), disable_notification=1)
-        bot.send_audio(message.chat.id, random.choice(Rock_id), disable_notification=1)
-        bot.send_audio(message.chat.id, random.choice(Rock_id), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(rock_list), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(rock_list), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(rock_list), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(rock_list), disable_notification=1)
+        bot.send_audio(message.chat.id, random.choice(rock_list), disable_notification=1)
         #bot.send_sticker(message.chat.id, '')
 
     elif message.text.lower() == 'r&b':
@@ -275,9 +188,6 @@ def send_text(message):
         #bot.send_sticker(message.chat.id, '')
 
     elif message.text.lower() == 'электроника':
-        bot.send_audio(message.chat.id, random.choice(), disable_notification=1)
-        bot.send_audio(message.chat.id, random.choice(), disable_notification=1)
-        bot.send_audio(message.chat.id, random.choice(), disable_notification=1)
         bot.send_audio(message.chat.id, random.choice(), disable_notification=1)
         bot.send_audio(message.chat.id, random.choice(), disable_notification=1)
         #bot.send_sticker(message.chat.id, '')
